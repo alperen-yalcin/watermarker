13504c8551129b86706035f2d5a2a8d9cc02a50f
Alperen Yalcin <alperenyalcin212@gmail.com>
2025-08-31 21:19:09 +0300

    feat(video-processing): implement watermak overlay using MoviePy

---
+++ main.py
@@ -0,0 +1,15 @@
+from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
+import os
+os.environ["IMAGEMAGICK_BINARY"] = "/opt/homebrew/bin/magick"  
+
+
+video = VideoFileClip("./video/watermarked.mp4")
+
+watermark = TextClip("@geceyefisildayan", fontsize=40, color="white", font="Arial-Bold")
+watermark = watermark.set_duration(video.duration)  
+watermark = watermark.set_pos(("right","bottom"))   
+watermark = watermark.set_opacity(0.6)             
+
+final = CompositeVideoClip([video, watermark])
+
+final.write_videofile("video_watermarked.mp4", codec="libx264", audio_codec="aac")
